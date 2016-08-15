dc0441be0ba29603aaad2c5c632473d1089930a6
juliusmagnanimus <juliucjpe@gmail.com>
2016-08-15 17:16:13 -0500

    se sube el primer proyecto de php

---
+++ holaMundo2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <meta charset="UTF-8"> 
+  <title>Unidades de medida</title>
+  </head>
+
+<body>
+    <?php 
+   echo "Hola mundo";
+   ?>
+      
+</body>
+</html>
